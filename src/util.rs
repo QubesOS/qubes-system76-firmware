463496699018cf2681b030d94b19334f49f1ec4d
Demi Marie Obenour <demi@invisiblethingslab.com>
2024-06-16 16:36:16 -0400

    Use a tar subprocess instead of pure-Rust implementation
    This avoids two dependencies.  Not tested!

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,7 @@
-use lzma::reader::LzmaReader;
 use sha2::{Digest, Sha256};
-use std::io::Read;
+use std::ffi::OsStr;
+use std::io::{Read, Write};
 use std::{fs, io, path, process};
-use tar::Archive;
 
 pub fn get_efi_mnt() -> Option<String> {
     let bootctl_esp = process::Command::new("bootctl")
@@ -27,49 +26,47 @@ pub fn get_efi_mnt() -> Option<String> {
 }
 
 pub fn extract<P: AsRef<path::Path>>(data: &[u8], p: P) -> io::Result<()> {
-    let decompressor = LzmaReader::new_decompressor(data)
-        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-    let mut tar = Archive::new(decompressor);
-
-    for file_res in tar.entries()? {
-        let mut file = file_res?;
-
-        println!("{:?}", file.path());
-        if !file.unpack_in(&p)? {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("invalid file path {:?}", file.path()),
-            ));
-        }
+    let mut extract_cli = process::Command::new("tar")
+        .args(["-xJv"])
+        .stdin(process::Stdio::piped())
+        .current_dir(p)
+        .spawn()?;
+    let mut stdin = extract_cli.stdin.take().expect("stdlib bug");
+    stdin.write_all(data)?;
+    let output = extract_cli.wait()?;
+    if output.success() {
+        Ok(())
+    } else {
+        Err(io::Error::new(io::ErrorKind::Other, "tar failed"))
     }
-
-    Ok(())
 }
 
 pub fn extract_file<P: AsRef<path::Path>>(data: &[u8], path: P) -> io::Result<String> {
-    let decompressor = LzmaReader::new_decompressor(data)
-        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-    let mut tar = Archive::new(decompressor);
+    let mut extract_cli = process::Command::new("tar")
+        .args([
+            OsStr::new("-xJvO"),
+            OsStr::new("--"),
+            path.as_ref().as_os_str(),
+        ])
+        .stdin(process::Stdio::piped())
+        .stdout(process::Stdio::piped())
+        .spawn()?;
+    let mut stdin = extract_cli.stdin.take().expect("stdlib bug");
+    let mut stdout = extract_cli.stdout.take().expect("stdlib bug");
+    let output = std::thread::scope(|scope| {
+        let res = scope.spawn(move || stdin.write_all(data));
 
-    for file_res in tar.entries()? {
-        let mut file = file_res?;
-
-        if let Ok(file_path) = file.path() {
-            if file_path != path.as_ref() {
-                continue;
-            }
-        }
-
-        println!("{:?}", file.path());
-        let mut s = String::new();
-        file.read_to_string(&mut s)?;
-        return Ok(s);
+        let mut output = String::new();
+        stdout.read_to_string(&mut output)?;
+        res.join().expect("thread panicked")?;
+        Ok::<_, std::io::Error>(output)
+    })?;
+    let status = extract_cli.wait()?;
+    if status.success() {
+        Ok(output)
+    } else {
+        Err(io::Error::new(io::ErrorKind::Other, "tar failed"))
     }
-
-    Err(io::Error::new(
-        io::ErrorKind::NotFound,
-        format!("failed to find {:?}", path.as_ref()),
-    ))
 }
 
 pub fn read_string<P: AsRef<path::Path>>(p: P) -> io::Result<String> {
